d3efe0eb6364f4baf8fa1f1069bba9f6c96a2e7a
tomekceszke <tomekceszke@users.noreply.github.com>
2017-09-01 00:20:21 +0200

    passat b6 case study

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # passat-b6-tdi
 Want to buy or sell a car? What is adequate price? Machine learning to the rescue!
 
-[![passat](passat.jpg)](predict.ipynb) 
+[![passat](passat.jpg)](passat_b6_tdi_price_prediction.ipynb) 
 
 This ML algorithm written in Python uses scikit-learn framework to predict adequate price for an iconic Volkswagen Passat B6 TDI.
 As a data source (train&test) it uses offers from Poland's biggest online vehicle marketplace. 
@@ -14,6 +14,6 @@ To predict the best price you will need to give 3 features:
 
 
  ## Run
- * Just open this interactive [IPython Notebook file](predict.ipynb) to check the results for preset values
+ * Just open this interactive [IPython Notebook file](passat_b6_tdi_price_prediction.ipynb) to check the results for preset values
   
  
--- car_price_prediction.py
+++ car_price_prediction.py
@@ -2,18 +2,32 @@ import urllib.request
 
 import numpy as np
 from bs4 import BeautifulSoup
+from enum import Enum
 
 
-def collect_data(main_url):
+class Capacity(Enum):
+    cap_1_6 = 1598
+    cap_1_9 = 1896
+    cap_2_0 = 1968
+
+def get_html(url):
+    html = urllib.request.urlopen(url).read()
+    soup = BeautifulSoup(html, "lxml")
+    return soup
+
+
+def count_pages(main_url):
+    soup = get_html(main_url)
+    return int(soup.find_all('span', attrs={'class': 'page'})[-2].text)
+
+
+def collect_data(main_url, pages=3):
     Xy = np.empty((0, 4))
-    htm = None
-    for page in range(1, 10):  # pages+1):
+    for page in range(1, pages + 1):
+        print('\rParsing page: ' + str(page), end='')
         url = main_url + '&page=' + str(page)
-        html = urllib.request.urlopen(url).read()
-        soup = BeautifulSoup(html, "lxml")
-
+        soup = get_html(url)
         articles = soup.find_all('article')
-        print('Page: ' + str(page) + ' | Offers: ' + str(len(articles)))
 
         for article in articles:
             # year
@@ -30,36 +44,26 @@ def collect_data(main_url):
             capacity_tag = article.find("li", attrs={'data-code': 'engine_capacity'})
             if (capacity_tag == None):
                 continue
-            capacity = int(capacity_tag.span.text.replace(' ', '')[:-3])
-            if capacity < 1896:
-                capacity = 1598
-            elif capacity >= 1896 and capacity < 1968:
-                capacity = 1896
-            else:
-                capacity = 1968
+            capacity = parse_capacity(int(capacity_tag.span.text.replace(' ', '')[:-3]))
             # price
             price_tag = article.find("span", attrs={'class': 'offer-price__number'})
             if (price_tag == None):
                 continue
             price = int(price_tag.contents[0].replace(' ', ''))
             Xy = np.append(Xy, [[year, mileage, capacity, price]], axis=0)
+    print('\n')
     return Xy
 
 
 def split_data(Xy, percent_of_test_data=30):
     n = len(Xy)
-    print('\nTotal samples: ', n)
-
     # print(Xv[-10:])
-    np.random.shuffle(Xy)
+    # np.random.shuffle(Xy)
 
     n_train = round((100 - percent_of_test_data) / 100 * n)
     n_test = n - n_train
 
     [Xy_train, Xy_test, _] = np.split(Xy, [n_train, n_train + n_test])
-
-    print('Training samples: %d' % len(Xy_train))
-    print('Test samples: %d' % len(Xy_test))
 
     X_train = Xy_train[:, [0, 1, 2]]
     y_train = Xy_train[:, [3]]
@@ -68,3 +72,14 @@ def split_data(Xy, percent_of_test_data=30):
     y_test = Xy_test[:, [3]]
 
     return X_train, y_train, X_test, y_test
+
+
+def parse_capacity(capacity):
+    if capacity < Capacity.cap_1_9.value:
+        capacity = Capacity.cap_1_6.value
+    elif capacity >= Capacity.cap_1_9.value and capacity < Capacity.cap_2_0.value:
+        capacity = Capacity.cap_1_9.value
+    else:
+        capacity = Capacity.cap_2_0.value
+
+    return capacity
